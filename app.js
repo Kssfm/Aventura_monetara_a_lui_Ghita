3437f8b7a169480d6deb818a60a295004ef1a0b7
Kssfm <mihai.farcasu09@gmail.com>
2026-07-29 07:55:46 +0300

    int

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*game code*/
-const gameFrame = document.getElementById('gameFrame');
+const gameFrame = document.getElementById('gameFrame_iframe');
 const loadingScreen = document.getElementById('loadingScreen');
 const fullscreenBtn = document.getElementById('fullscreenBtn');
 const playerWrapper = document.getElementById('playerWrapper');
